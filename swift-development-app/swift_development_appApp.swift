3b368247ca88e1fa61e90204154e735f7dd09d55
emildsn <emildsn00@gmail.com>
2023-03-02 15:04:16 -0300

    Initial Commit

---
+++ swift-development-app/swift_development_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_development_appApp.swift
+//  swift-development-app
+//
+//  Created by Emiliano Diaz on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_development_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
