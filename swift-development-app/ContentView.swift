d040dd2f9d869ac9bed8a58b22a051cc43962e8a
emildsn <emildsn00@gmail.com>
2023-03-05 17:30:31 -0300

    add size class

--- swift-development-app/ContentView.swift
+++ swift-development-app/ContentView.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var show = false
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
+    var body: some View {
+        if sizeClass == .compact {
+            compactDesign()
+        } else {
+            regularDesign()
+        }
+    }
+}
+
+struct compactDesign: View {
     var body: some View {
         ZStack {
             Color.red.ignoresSafeArea(.all)
+            VStack {
+                Image("appstore").resizable()
+                    .frame(width: 100,
+                           height: 100,
+                           alignment: .center)
+                    .clipShape(Circle())
+                Text("Hello World")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .bold()
+            }
+        }
+    }
+}
+
+struct regularDesign: View {
+    var body: some View {
+        ZStack {
+            Color.orange.ignoresSafeArea(.all)
             HStack {
                 Image("appstore").resizable()
                     .frame(width: 100,
                            height: 100,
                            alignment: .center)
+                    .clipShape(Circle())
                 Text("Hello World")
                     .font(.largeTitle)
                     .foregroundColor(.white)
